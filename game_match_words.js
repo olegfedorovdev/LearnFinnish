b5d8cd178696865fca07a23ab3b437ad5218b51f
Oleg Fedorov <bishop3000home@gmail.com>
2017-12-09 21:23:35 +0200

    selecting and showing works

--- game_match_words.js
+++ game_match_words.js
@@ -1,102 +1,108 @@
 
 Games["match_words"] = {
-    _currentWordIndex: 0,
-
-    _fiWord1: 0,//variant to choose 1 (index)
-    _fiWord2: 0,//variant to choose 2
-
     // "words" : [] - here will be words
-    _elementEnWord : 0,
-    _elementFiWord1 : 0,
-    _elementFiWord2: 0,
-    _elementImg: 0,
-    _elementSound: 0,
 
+    // elements for words 1..8
+    _elementEnWords : [],
+    _elementFiWords : [],
+
+    _currentWordsFi: [],//shuffled array of words which are shown
+    _currentWordsEn: [],//shuffled array of words which are shown
+
+    _selectedWordFi: undefined,//index
+    _selectedWordEn: undefined,//index
+    
     "start" : function() {
         console.log("Start game choose_word");
-        this._currentWordIndex = Math.floor(Math.random() * this.words.length);
-
-        this._elementEnWord = document.getElementById("choose_word_en");
-        this._elementFiWord1 = document.getElementById("choose_word_fi_1");
-        this._elementFiWord2 = document.getElementById("choose_word_fi_2");
-        this._elementImg = document.getElementById("choose_word_img");
-        this._elementSound = document.getElementById("choose_word_sound");
-
-        this.showCurrentWord();
         var that = this;
+        for(let i = 1; i <= 8; ++i) {
+            this._elementEnWords[i] = document.getElementById("match_words_en_" + i);
+            this._elementFiWords[i] = document.getElementById("match_words_fi_" + i);
+            this._elementEnWords[i].onclick = function() {that.onWordSelectedEn(i);}
+            this._elementFiWords[i].onclick = function() {that.onWordSelectedFi(i);}
+            this.unselect(this._elementFiWords[i]);
+            this.unselect(this._elementEnWords[i]);
+        }
+        this._currentWordsFi = JSON.parse(JSON.stringify(this.words));//copy
+        this._currentWordsFi = this.shuffle(this._currentWordsFi);
+        this._currentWordsEn = JSON.parse(JSON.stringify(this.words));//copy
+        this._currentWordsEn = this.shuffle(this._currentWordsEn);
+        this._selectedWordFi = undefined;
+        this._selectedWordEn = undefined;
 
-        this._elementFiWord1.onclick = function() {that.onWordSelected(1);}
-        this._elementFiWord2.onclick = function() {that.onWordSelected(2);}
+        this.showWords();
     },
     "stop" : function() {
-        console.log("Stop game choose_word");
-        this._elementSound.src = "";
+        console.log("Stop game match words");
     },
     
     "onPrevious": function() {
-        this._currentWordIndex--;
-        if (this._currentWordIndex < 0) {
-            this._currentWordIndex = this.words.length - 1;
-        }
-        this.showCurrentWord();
     },
 
     "onNext": function() {
-        this._currentWordIndex++;
-        if (this._currentWordIndex >= this.words.length) {
-            this._currentWordIndex = 0;
-        }
-        this.showCurrentWord();
     },
 
-    "onWordSelected": function(_wordIndex) {
-        console.log("Selected: " + _wordIndex);
-        let wordIndex = (_wordIndex == 1)?this._fiWord1:this._fiWord2;
-        if (wordIndex === this._currentWordIndex) {
-            this._elementSound.src = words.getAudioSrc(this.words[wordIndex]);
-            this._elementSound.play();
-            this.win(_wordIndex == 1?this._elementFiWord1:this._elementFiWord2);
-        } else {
-            this.lose(_wordIndex == 1?this._elementFiWord1:this._elementFiWord2)
-        }
-    },
-
-    "win" : function(element) {
-        element.style.color = "green";
+    "win" : function() {
         var that = this;
-        setTimeout(function() {that.onNext()}, 1000);
+        setTimeout(function() {that.start()}, 1000);
     },
 
-    "lose": function(element) {
-        element.style.color = "red";
-    },
-
-    // shows current word and 2 choices
-    "showCurrentWord" : function() {
-        if (Math.random() < 0.5) {
-            this._fiWord1 = this._currentWordIndex;
-            this._fiWord2 = this.getRandomWord(this._fiWord1);
-        } else {
-            this._fiWord2 = this._currentWordIndex;
-            this._fiWord1 = this.getRandomWord(this._fiWord2);
+    "showWords" : function() {
+        if (this._currentWordsFi.length === 0) {
+            this.win();
         }
-
-        this._elementEnWord.textContent = this.words[this._currentWordIndex].en;
-        this._elementFiWord1.textContent = this.words[this._fiWord1].fi;
-        this._elementFiWord1.style.color = "black";
-        this._elementFiWord2.textContent = this.words[this._fiWord2].fi;
-        this._elementFiWord2.style.color = "black";
-        this._elementImg.src = words.getImgSrc(this.words[this._currentWordIndex]);
-
+        for(let i = 1; i <= 8; ++i) {
+            if (i >= this._currentWordsFi.length) {
+                this._elementEnWords[i].hidden = true;
+                this._elementFiWords[i].hidden = true;
+            } else {
+                this._elementEnWords[i].hidden = false;
+                this._elementFiWords[i].hidden = false;
+                this._elementEnWords[i].textContent = this._currentWordsEn[i].en;
+                this._elementFiWords[i].textContent = this._currentWordsFi[i].fi;
+            }
+        }
     },
 
-    "getRandomWord": function(excludeWord) {
-        do {
-            let r = Math.floor(Math.random() * this.words.length);
-            if (r != excludeWord) {
-                return r;
-            }
-        } while(true);
+    "onWordSelectedEn": function(wordIndex) {
+        console.log("Selected en: " + wordIndex);
+        if (this._selectedWordEn !== undefined) {
+            this.unselect(this._elementEnWords[this._selectedWordEn])
+        }
+        this._selectedWordEn = wordIndex;
+        this.select(this._elementEnWords[this._selectedWordEn])
+    },
+    "onWordSelectedFi": function(wordIndex) {
+        console.log("Selected fi: " + wordIndex);
+        if (this._selectedWordFi !== undefined) {
+            this.unselect(this._elementFiWords[this._selectedWordFi])
+        }
+        this._selectedWordFi = wordIndex;
+        this.select(this._elementFiWords[this._selectedWordFi])
+    },
+
+    "select" : function(element) {
+        element.style["background-color"] = "#ffe0d0";
+    },
+
+    "unselect" : function(element) {
+        element.style["background-color"] = "white";
+    },
         
+    // shuffle array
+    "shuffle" : function(array) {
+        let counter = array.length;
+    
+        while (counter > 0) {
+            let index = Math.floor(Math.random() * counter);
+            counter--;
+
+            let temp = array[counter];
+            array[counter] = array[index];
+            array[index] = temp;
+        }
+    
+        return array;
     }
+
 }
