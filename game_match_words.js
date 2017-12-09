81cfd7195d5364f771bda2bb79fcd457fe3a524b
Oleg Fedorov <bishop3000home@gmail.com>
2017-12-09 22:41:41 +0200

    match words works

--- game_match_words.js
+++ game_match_words.js
@@ -25,8 +25,10 @@ Games["match_words"] = {
         }
         this._currentWordsFi = JSON.parse(JSON.stringify(this.words));//copy
         this._currentWordsFi = this.shuffle(this._currentWordsFi);
-        this._currentWordsEn = JSON.parse(JSON.stringify(this.words));//copy
-        this._currentWordsEn = this.shuffle(this._currentWordsEn);
+        this._currentWordsEn = JSON.parse(JSON.stringify(this._currentWordsFi));//copy
+        this._currentWordsEn = this.shuffle_nearby(this._currentWordsEn);
+        this._currentWordsFi.unshift({});
+        this._currentWordsEn.unshift({});
         this._selectedWordFi = undefined;
         this._selectedWordEn = undefined;
 
@@ -48,9 +50,11 @@ Games["match_words"] = {
     },
 
     "showWords" : function() {
-        if (this._currentWordsFi.length === 0) {
+        // we have unused element 0 so length 1 means nothing left
+        if (this._currentWordsFi.length === 1) {
             this.win();
         }
+
         for(let i = 1; i <= 8; ++i) {
             if (i >= this._currentWordsFi.length) {
                 this._elementEnWords[i].hidden = true;
@@ -70,15 +74,68 @@ Games["match_words"] = {
             this.unselect(this._elementEnWords[this._selectedWordEn])
         }
         this._selectedWordEn = wordIndex;
-        this.select(this._elementEnWords[this._selectedWordEn])
+        this.select(this._elementEnWords[this._selectedWordEn]);
+
+        var that = this;
+        setTimeout(function() {that.checkSelectedWords()}, 500);
     },
+
     "onWordSelectedFi": function(wordIndex) {
         console.log("Selected fi: " + wordIndex);
         if (this._selectedWordFi !== undefined) {
             this.unselect(this._elementFiWords[this._selectedWordFi])
         }
         this._selectedWordFi = wordIndex;
-        this.select(this._elementFiWords[this._selectedWordFi])
+        this.select(this._elementFiWords[this._selectedWordFi]);
+
+        var that = this;
+        setTimeout(function() {that.checkSelectedWords()}, 500);
+    },
+
+    "checkSelectedWords": function() {
+        if (this._selectedWordEn === undefined || this._selectedWordFi == undefined) {
+            return;
+        }
+        let en = this._currentWordsEn[this._selectedWordEn];
+        let fi = this._currentWordsFi[this._selectedWordFi];
+
+        if (en.fi === fi.fi) {
+            this.removeSelectedWords();
+        } else {
+            this.markSelectedWordsWrong();
+        }
+        this.showWords();
+    },
+
+    "removeSelectedWords" : function() {
+        console.log("Guessed right");
+        this.unselect(this._elementFiWords[this._selectedWordFi]);
+        this.unselect(this._elementEnWords[this._selectedWordEn]);
+        this._currentWordsEn.splice(this._selectedWordEn, 1);
+        this._currentWordsFi.splice(this._selectedWordFi, 1);
+        this._selectedWordEn = undefined;
+        this._selectedWordFi = undefined;
+    },
+
+    "markSelectedWordsWrong" : function() {
+        console.log("Guessed wrong");
+        var en = this._elementEnWords[this._selectedWordEn];
+        var fi = this._elementFiWords[this._selectedWordFi];
+        this.markWrong(en);
+        this.markWrong(fi);
+        
+        var that = this;
+        setTimeout(function() {
+            that.unselect(fi);
+            that.unselect(en);
+        }, 1100);
+
+        this._selectedWordEn = undefined;
+        this._selectedWordFi = undefined;
+    },
+
+    "markWrong" : function(element) {
+        element.style["background-color"] = "#ff1010";
     },
 
     "select" : function(element) {
@@ -100,6 +157,30 @@ Games["match_words"] = {
             let temp = array[counter];
             array[counter] = array[index];
             array[index] = temp;
+        }
+    
+        return array;
+    },
+
+    // shuffle array leaving entries still nearby
+    "shuffle_nearby" : function(array) {
+        let counter = array.length;
+    
+        while (counter > 0) {
+            let rnd = Math.floor(Math.random() * 8);
+            counter--;
+            
+            let idx = counter + 4 - rnd;
+            if (idx < 0) {
+                idx = 0;
+            }
+            if (idx >= array.length) {
+                idx = array.length - 1;
+            }
+            
+            let temp = array[counter];
+            array[counter] = array[idx];
+            array[idx] = temp;
         }
     
         return array;
