7467727c64e01fa686303b3724139837ccc39277
Oleg Fedorov <bishop3000home@gmail.com>
2017-12-12 21:23:07 +0200

    added qiestions

---
+++ questions.js
@@ -0,0 +1,20 @@
+words["questions"] = {
+    "type": "Questions",
+    "words" : [
+        {"fi":"Mikä", "en":"What (concrete or food)", "ru": "Что? конкретное или еда"},
+        {"fi":"Mitä", "en":"What (something)", "ru": "Что? неконкретное"},
+        {"fi":"Missä", "en":"Where", "ru": "Где?"},
+        {"fi":"Kuka", "en":"Who", "ru": "Кто?"},
+        {"fi":"Miksi", "en":"Why", "ru": "Почему?"},
+        {"fi":"Milloin", "en":"When", "ru": "Когда?"},
+        {"fi":"Kenen", "en":"Whoose", "ru": "Чей?"},
+        {"fi":"Kumpi", "en":"Which one", "ru": "Какой из?"},
+        {"fi":"Mihin", "en":"Where to", "ru": "Куда?"},
+        {"fi":"Kuinka", "en":"How many", "ru": "Сколько?"},
+        {"fi":"Mistä", "en":"From where?", "ru": "Откуда?"},
+        {"fi":"Millainen", "en":"How is...?", "ru": "Какой, какая?"},
+        {"fi":"Minkä", "en":"Which", "ru": "Который, какие?"},
+        {"fi":"Miten", "en":"How", "ru": "Как?"},
+        {"fi":"Minne", "en":"Where to", "ru": "Куда?"},
+    ]
+};
