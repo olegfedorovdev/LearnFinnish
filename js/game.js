de2ce7a9c63338f415b281377a05d39323d3e2f5
Oleg Fedorov <bishop3000home@gmail.com>
2017-12-25 23:11:57 +0200

    prevent scrolling on touch

--- js/game.js
+++ js/game.js
@@ -8,9 +8,9 @@ function startGame() {
 
     let gameField = document.getElementById('gameFields');
     document.onkeydown = function(e) {helpers.handleKeyDown(e)};
-    gameField.addEventListener("touchstart", function(e) {helpers.handleTouchStart(e)}, true);
-    gameField.addEventListener("touchmove", function(e) {helpers.handleTouchMove(e)}, true);
-    gameField.addEventListener("touchend", function(e) {helpers.handleTouchEnd(e)}, true);
+    gameField.addEventListener("touchstart", function(e) {e.preventDefault();helpers.handleTouchStart(e)}, true);
+    gameField.addEventListener("touchmove", function(e) {e.preventDefault();helpers.handleTouchMove(e)}, true);
+    gameField.addEventListener("touchend", function(e) {e.preventDefault();helpers.handleTouchEnd(e)}, true);
     gameField.addEventListener("touchcancel", function(e) {helpers.handleTouchCancel(e)}, true);
 
     showMainMenu();
