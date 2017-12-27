fc34d0a66d5fa943f1397e9ff0f2b597c92d534a
Oleg Fedorov <bishop3000home@gmail.com>
2017-12-27 21:39:52 +0200

    added months and seasons

---
+++ js/words/seasons.js
@@ -0,0 +1,20 @@
+words["seasons"] = {
+    "type": "Seasons&Months",
+    "words" : [
+        {"fi":"tammikuu", "en":"January", "ru": "январь"},
+        {"fi":"helmikuu", "en":"February", "ru": "февраль"},
+        {"fi":"maaliskuu", "en":"March", "ru": "март"},
+        {"fi":"huhtikuu", "en":"April", "ru": "апрель"},
+        {"fi":"toukokuu", "en":"May", "ru": "май"},
+        {"fi":"kesäkuu", "en":"June", "ru": "июнь"},
+        {"fi":"heinäkuu", "en":"July", "ru": "июль"},
+        {"fi":"elokuu", "en":"August", "ru": "август"},
+        {"fi":"syyskuu", "en":"September", "ru": "сентябрь"},
+        {"fi":"marraskuu", "en":"November", "ru": "ноябрь"},
+        {"fi":"joulukuu", "en":"December", "ru": "декабрь"},
+        {"fi":"talvi", "en":"Winter", "ru": "зима"},
+        {"fi":"kevät", "en":"Spring", "ru": "весна"},
+        {"fi":"kesä", "en":"Summer", "ru": "лето"},
+        {"fi":"syksy", "en":"Autumn", "ru": "осень"}        
+    ]
+};
