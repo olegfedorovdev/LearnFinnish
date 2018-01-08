23c5cc5bf4451f5447bd184e060e02fd5d6ff027
Oleg Fedorov <bishop3000home@gmail.com>
2018-01-08 23:11:08 +0200

    new set 10

---
+++ js/words/kappale10.js
@@ -0,0 +1,24 @@
+words["kappale10"] = {
+    "type": "Kappale 10",
+    "words" : [
+        {"fi":"joukko", "en":"set", "ru": "множество"},
+        {"fi":"tyhjä", "en":"empty", "ru": "пустой"},
+        {"fi":"likainen", "en":"dirty", "ru": "грязный"},
+        {"fi":"enää", "en":"anymore", "ru": "больше..."},
+        {"fi":"jaksaa", "en":"to keep doing", "ru": "продолжать делать"},
+        {"fi":"kevyt", "en":"light", "ru": "легкий"},
+        {"fi":"luulla", "en":"to guess, to suppose", "ru": "предполагать"},
+        {"fi":"tutkia", "en":"to examine, to explore", "ru": "изучать, исследовать"},
+        {"fi":"ehkä", "en":"perhaps", "ru": "возможно"},
+        {"fi":"esitellä", "en":"to demonstrate", "ru": "демонстрировать"},
+        {"fi":"neuvotella", "en":"to negotiate", "ru": "договариваться"},
+        {"fi":"tuntea", "en":"to know someone, to feel", "ru": "знать кого-то"},
+        {"fi":"pyytää", "en":"to request, to ask", "ru": "спрашивать"},
+        {"fi":"patsas", "en":"statue", "ru": "статуя"},
+        {"fi":"pensas", "en":"bush", "ru": "куст"},
+        {"fi":"kiva", "en":"nice", "ru": "хороший"},
+        {"fi":"petata", "en":"to make a bed", "ru": "заправлять кровать"},
+        {"fi":"mieli", "en":"mind", "ru": "разум"},
+        {"fi":"mielipide", "en":"opinion", "ru": "мнение"}
+    ]
+};
