f5c89c51223c7e764335d1881f9bb5e103a91b42
Oleg Fedorov <bishop3000home@gmail.com>
2018-01-26 22:53:32 +0200

    new words

--- js/words/kappale2_2.js
+++ js/words/kappale2_2.js
@@ -25,20 +25,6 @@ words["kappale2_2"] = {
         {"fi":"eksyä", "en":"to get lost", "ru": "теряться"},
         {"fi":"toivo", "en":"hope", "ru": "надежда"},
         {"fi":"toivoa", "en":"to hope", "ru": "надеяться"},
-        {"fi":"päättää", "en":"to decide, to choose", "ru": "выбирать, решать"},
-
-
-        {"fi":"huomata", "en":"to notice, to note", "ru": "замечать"},
-        {"fi":"toimittaa", "en":"to deliver", "ru": "доставлять"},
-        {"fi":"suositella", "en":"to recommend", "ru": "рекоммендовать"},
-        {"fi":"möllöttää", "en":"to gape", "ru": "зевать"},
-        {"fi":"taipua", "en":"to bend, to yield", "ru": "гнуть, сдаваться"},
-        {"fi":"huoli", "en":"to worry, to fear", "ru": "волноваться"},
-        {"fi":"selkeä", "en":"clear, self-explanatory", "ru": "понятный"},
-        {"fi":"telkkaria", "en":"TV", "ru": "телевизор"},
-        {"fi":"hemmetti", "en":"hell", "ru": "ад"},
-        {"fi":"pyhä", "en":"holy", "ru": "святой"},
-        {"fi":"unelma", "en":"dream", "ru": "мечта"},
-        {"fi":"älä", "en":"do not...", "ru": "не..."}
+        {"fi":"päättää", "en":"to decide, to choose", "ru": "выбирать, решать"}
     ]
 };
---
+++ js/words/kappale2_2_2.js
@@ -0,0 +1,23 @@
+words["kappale2_2_2"] = {
+    "type": "Kappale 2-2-2",
+    "words" : [
+        {"fi":"huomata", "en":"to notice, to note", "ru": "замечать"},
+        {"fi":"toimittaa", "en":"to deliver", "ru": "доставлять"},
+        {"fi":"suositella", "en":"to recommend", "ru": "рекоммендовать"},
+        {"fi":"möllöttää", "en":"to gape", "ru": "зевать"},
+        {"fi":"taipua", "en":"to bend, to yield", "ru": "гнуть, сдаваться"},
+        {"fi":"huoli", "en":"to worry, to fear", "ru": "волноваться"},
+        {"fi":"selkeä", "en":"clear, self-explanatory", "ru": "понятный"},
+        {"fi":"telkkaria", "en":"TV", "ru": "телевизор"},
+        {"fi":"hemmetti", "en":"hell", "ru": "ад"},
+        {"fi":"pyhä", "en":"holy", "ru": "святой"},
+        {"fi":"unelma", "en":"dream", "ru": "мечта"},
+        {"fi":"älä", "en":"do not...", "ru": "не..."},
+        {"fi":"ratkaistu", "en":"settled", "ru": "решен"},
+        {"fi":"korvaus", "en":"compensation", "ru": "компенсация"},
+        {"fi":"kauhea", "en":"terible", "ru": "ужасная"},
+        {"fi":"tilanne", "en":"situation", "ru": "ситуация"},
+        {"fi":"kiltti", "en":"kind", "ru": "хороший, добрый"},
+        {"fi":"kustannus", "en":"cost", "ru": "цена, стоимость"}
+    ]
+};
