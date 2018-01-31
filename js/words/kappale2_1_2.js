018cd4f8642927752139864c7b72386c26e53329
Oleg Fedorov <bishop3000home@gmail.com>
2018-01-31 23:10:26 +0200

    new words

---
+++ js/words/kappale2_1_2.js
@@ -0,0 +1,28 @@
+words["Words 31.01"] = {
+    "type": "Words 31.01",
+    "words" : [
+        {"fi":"verrata", "en":"to compare", "ru": "доставлять"},
+        {"fi":"huuhtoa", "en":"to flush, to rinse", "ru": "доставлять"},
+        {"fi":"tarttua", "en":"to grab, to catch, to stick", "ru": "доставлять"},
+        {"fi":"tarttua", "en":"to be infectious, be contagious", "ru": "доставлять", "img":"img/tarttua.png"},
+        {"fi":"halata", "en":"to hug", "ru": "доставлять"},
+        {"fi":"tottua", "en":"to get used to", "ru": "доставлять"},
+
+        {"fi":"haitta", "en":"disadvantage, hindrance", "ru": "замечать"},
+        {"fi":"kirkas", "en":"bright", "ru": "доставлять"},
+        {"fi":"hajuton", "en":"odorless", "ru": "доставлять"},
+        {"fi":"haju", "en":"smell", "ru": "доставлять"},
+        {"fi":"tottumus", "en":"habit", "ru": "доставлять"},
+        {"fi":"pisin", "en":"longest", "ru": "доставлять"},
+        {"fi":"maisema", "en":"landscape, view", "ru": "доставлять"},
+        {"fi":"suosittu", "en":"popular", "ru": "доставлять"},
+        {"fi":"kättely", "en":"handshake", "ru": "доставлять"},
+        {"fi":"puristaa", "en":"squeeze", "ru": "доставлять"},
+        {"fi":"työhaastattelu", "en":"job interview", "ru": "доставлять"},
+        {"fi":"haastattelu", "en":"interview", "ru": "доставлять"},
+        {"fi":"tuttu", "en":"known", "ru": "доставлять"},        
+        {"fi":"etenkin", "en":"especially", "ru": "доставлять"},
+        {"fi":"suukko", "en":"a kiss", "ru": "доставлять"},
+        {"fi":"nyrkki", "en":"fist", "ru": "доставлять"}        
+    ]
+};
